e343927aed3eeb6ad529c1538545d5f1c285da99
Brooks Van Buren <woofiewilly@gmail.com>
2016-09-07 13:31:28 -0400

    Fixed <img> formatting and template, resolved card.js issues and removed debug statements

--- js/card.js
+++ js/card.js
@@ -1,7 +1,6 @@
-$(document).on('ready', function() {
+$(document).ready( function() {
 
   $('.container').on('click', function() {
-    console.log('fire');
     var card = $(this).find('.card');
     if (card.css('transform') === 'matrix3d(-1, 0, -1.22465e-16, 0, 0, 1, 0, 0, 1.22465e-16, 0, -1, 0, 0, 0, 0, 1)') {
       card.css('transform', 'rotateY(0deg)');
